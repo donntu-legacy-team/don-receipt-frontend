83e67d839a443befafb507f49db6a59b9b5fb2cf
Alexandr Shiriaev <alexandrshiriaev7@gmail.com>
2025-04-13 13:07:44 +0300

    review fixes

--- src/shared/ui/logo/logo.tsx
+++ src/shared/ui/logo/logo.tsx
@@ -1,7 +1,25 @@
 'use client'
-export const Logo = () => {
+
+import Link from 'next/link'
+
+export type LogoProps = {
+	href?: string 
+}
+
+export const Logo = ({ href }: LogoProps) => {
+
+	if (href) {
+		return (
+			<Link href={href}>
+				<div className="text-2xl font-bold">
+					ДонРецепты.ру
+				</div>
+			</Link>
+		)
+	}
+
 	return (
-		<div className="text-2xl font-bold" onClick={(event) => console.log(event)}>
+		<div className="text-2xl font-bold">
 			ДонРецепты.ру
 		</div>
 	)
